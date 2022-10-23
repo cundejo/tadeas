86539f8d36dfd72f24a03a225fe4065506fbc543
Oliver Sosa <cundejo85@gmail.com>
2022-10-23 18:31:33 -0500

    Added component to undo last completed task

---
+++ src/features/task/components/CompletedNotification.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card, Row, styled } from '@nextui-org/react';
+import { Task } from '@/features/task';
+import { Button } from '@/features/common';
+
+type Props = {
+  tasksCompletedRecently?: Task;
+  onUndo: (task: Task) => void;
+};
+
+export const CompletedNotification: React.FC<Props> = ({ tasksCompletedRecently, onUndo }) => {
+  if (!tasksCompletedRecently) return null;
+
+  return (
+    <Container>
+      <Card>
+        <Card.Body>
+          <Row align="center" css={{ justifyContent: 'space-between' }}>
+            <h4>Task completed</h4>
+            <Button auto flat onClick={() => onUndo(tasksCompletedRecently)}>
+              Undo
+            </Button>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+const Container = styled('div', {
+  bottom: '$md',
+  display: 'flex',
+  left: '$md',
+  margin: '0 auto',
+  maxWidth: '650px',
+  position: 'fixed',
+  right: '$md',
+});
