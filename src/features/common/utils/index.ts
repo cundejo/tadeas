ea951ecf36cb16825218dc606403c1ac9eb64c03
Oliver Sosa <cundejo85@gmail.com>
2022-10-24 22:52:46 -0500

    Added couple of sort functions

--- src/features/common/utils/index.ts
+++ src/features/common/utils/index.ts
@@ -7,3 +7,10 @@ export function removeUndefined<T extends object>(obj: T): T {
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export const booleanSort = (booleanA?: boolean, booleanB?: boolean): number => {
+  if (booleanA === booleanB) return 0;
+  return booleanA ? -1 : 1;
+};
+
+export const stringSort = (stringA?: string, stringB?: string): number => (stringA || '').localeCompare(stringB || '');
