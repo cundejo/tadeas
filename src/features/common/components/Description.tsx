0c604ea44b8d3e717f9c74bfc2be29c08c5cf02c
Oliver Sosa <cundejo85@gmail.com>
2022-11-04 18:00:17 -0500

    Improved Description

--- src/features/common/components/Description.tsx
+++ src/features/common/components/Description.tsx
@@ -1,16 +1,17 @@
-import { Container, Text } from '@nextui-org/react';
+import { Container, CSS, Text } from '@nextui-org/react';
 import { ReactNode } from 'react';
 
 interface Props {
   title: string | ReactNode;
   subTitle?: string | ReactNode;
   superTitle?: string | ReactNode;
+  css?: CSS;
 }
 
-export const Description: React.FC<Props> = ({ title, subTitle, superTitle }) => (
-  <Container css={{ padding: 0, margin: '1em 0' }}>
+export const Description: React.FC<Props> = ({ title, subTitle, superTitle, css }) => (
+  <Container css={{ padding: 0, margin: '1em 0', ...css }}>
     {superTitle && (
-      <Text css={{ margin: 0, color: '$accents7', textTransform: 'uppercase', fs: '$sm' }}>{superTitle}</Text>
+      <Text css={{ margin: 0, color: '$accents7', textTransform: 'uppercase', fs: '$xs' }}>{superTitle}</Text>
     )}
     <Text h4 css={{ margin: 0, fontWeight: 'normal', lineHeight: 'normal' }}>
       {title}
