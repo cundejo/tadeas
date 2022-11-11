abc09bd37b0f6e9f168f7ee6752113c05ca6f9ef
Oliver Sosa <cundejo85@gmail.com>
2022-11-11 16:25:08 -0600

    Removed title from home page

--- src/features/list/components/ListsDropdown.tsx
+++ src/features/list/components/ListsDropdown.tsx
@@ -2,7 +2,7 @@ import React, { Key, useState } from 'react';
 import { Dropdown, styled } from '@nextui-org/react';
 import { ListAddModal, useLists } from '@/features/list';
 import { MdCheck, MdPlaylistAdd } from 'react-icons/md';
-import { RootState, TextColorful } from '@/features/common';
+import { RootState } from '@/features/common';
 import { useSelector } from 'react-redux';
 import { isEmpty } from 'lodash';
 
@@ -33,9 +33,6 @@ export const ListsDropdown: React.FC = () => {
   return (
     <>
       <Container>
-        <h3>
-          <TextColorful>Tadeas</TextColorful>
-        </h3>
         <Dropdown>
           <Dropdown.Button light>{listSelected?.name}</Dropdown.Button>
           <Dropdown.Menu aria-label="My Lists" onAction={handleMenuAction} disabledKeys={[listSelected?.id as Key]}>
@@ -75,14 +72,6 @@ const EmptyIcon = styled('span', {
 });
 
 const Container = styled('nav', {
-  '& h3': {
-    m: 0,
-    fs: '$xs',
-    fontWeight: 700,
-    textTransform: 'uppercase',
-    letterSpacing: '0.03rem',
-  },
-
   '& button': {
     padding: 0,
     fs: '$lg',
