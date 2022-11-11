76eaff2a56c81b90cb4ce888c19f135bb81ea453
Oliver Sosa <cundejo85@gmail.com>
2022-11-11 16:24:44 -0600

    Fixed theme color in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,7 +24,7 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8e0978" />
           <meta name="msapplication-TileColor" content="#2b5797" />
           <meta name="msapplication-config" content="/browserconfig.xml" />
-          <meta name="theme-color" content="#2c2131" />
+          <meta name="theme-color" content="#000000" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="mobile-web-app-capable" content="yes" />
         </Head>
