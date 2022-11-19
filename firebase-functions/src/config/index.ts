ca4a530415e4152eeacdd88f340bc3cd85e42e85
Oliver Sosa <cano@fispan.com>
2022-11-18 23:48:54 -0600

    COnfigured the best wat to treat CORS without express

--- firebase-functions/src/config/index.ts
+++ firebase-functions/src/config/index.ts
@@ -6,4 +6,11 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccountObject as any),
 });
 
-export { admin };
+const ALLOWED_ORIGINS = [
+  'https://tadeas.app',
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  'http://192.168.0.20:3000',
+];
+
+export { admin, ALLOWED_ORIGINS };
