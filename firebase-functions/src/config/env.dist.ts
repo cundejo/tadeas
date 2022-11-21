a898f9c4490445f25369c4c70782c15d7fefe799
Oliver Sosa <cundejo85@gmail.com>
2022-11-20 23:04:49 -0600

    Removed env default values

--- firebase-functions/src/config/env.dist.ts
+++ firebase-functions/src/config/env.dist.ts
@@ -1,15 +1,15 @@
 // You get this from the Firebase project -> Project Settings -> Service accounts -> Firebase Admin SDK -> Generate new private key
 export const SERVICE_ACCOUNT_OBJECT = {
-  type: 'type',
-  project_id: 'project_id',
-  private_key_id: 'private_key_id',
-  private_key: 'private_key',
-  client_email: 'client_email',
-  client_id: 'client_id',
-  auth_uri: 'auth_uri',
-  token_uri: 'token_uri',
-  auth_provider_x509_cert_url: 'auth_provider_x509_cert_url',
-  client_x509_cert_url: 'client_x509_cert_url',
+  type: undefined,
+  project_id: undefined,
+  private_key_id: undefined,
+  private_key: undefined,
+  client_email: undefined,
+  client_id: undefined,
+  auth_uri: undefined,
+  token_uri: undefined,
+  auth_provider_x509_cert_url: undefined,
+  client_x509_cert_url: undefined,
 };
 
-export const SENDINGBLUE_API_KEY = 'SENDINGBLUE_API_KEY';
+export const SENDINGBLUE_API_KEY = undefined;
