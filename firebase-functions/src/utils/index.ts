6233935a0556c4ddd2594d3e5935070015d08900
Oliver Sosa <cundejo85@gmail.com>
2022-12-04 16:12:47 -0600

    Added verify authorization for secured functions requests

--- firebase-functions/src/utils/index.ts
+++ firebase-functions/src/utils/index.ts
@@ -1,6 +1,45 @@
+import { isUndefined, negate, pickBy } from 'lodash';
 import * as functions from 'firebase-functions';
 import * as cors from 'cors';
-import { ALLOWED_ORIGINS } from '../config';
+import { auth } from 'firebase-admin';
+import DecodedIdToken = auth.DecodedIdToken;
+import { admin, ALLOWED_ORIGINS } from '../config';
+
+export type Request = functions.https.Request;
+export type RequestSecure = functions.https.Request & { user: DecodedIdToken };
+export type Response<T> = functions.Response<T>;
+export type Func = (req: Request, res: Response<any>) => Promise<void>;
+export type FuncSecure = (req: RequestSecure, res: Response<any>) => Promise<void>;
+
+// Response helpers
+export const send = (res: Response<any>, code: string, data: string) => res.send({ code, data });
+
+export const onRequest = (func: FuncSecure) =>
+  functions.https.onRequest((req, res) => {
+    corsHandle(req, res, async () => verifyAuthorization(req, res, func));
+  });
+
+export const onInsecureRequest = (func: Func) =>
+  functions.https.onRequest((req, res) => {
+    corsHandle(req, res, func);
+  });
+
+// Handling CORS for every request is needed
+const corsHandle = (req: Request, res: Response<any>, func: any) => {
+  if (req.method === 'OPTIONS') {
+    // Allowing CORS in complex requests like pre-flight, https://www.npmjs.com/package/cors#enabling-cors-pre-flight
+    cors({ origin: true })(req, res, () => null);
+  } else {
+    cors(corsOptionsDelegate)(req, res, async () => {
+      if (isAllowedOrigin(req)) {
+        await func(req, res);
+      } else {
+        console.log('Request cancelled, origin denied');
+        res.status(404).send();
+      }
+    });
+  }
+};
 
 const isAllowedOrigin = (req: any): boolean => {
   const origin = req.header('Origin');
@@ -12,26 +51,26 @@ const corsOptionsDelegate = (req: any, callback: any) => {
   callback(null, { origin: isAllowedOrigin(req) });
 };
 
-/**
- * Currying functions.https.Request with CORS
- * @param {function} code
- * @return {function} functions.https.Request
- */
-export const onRequest = (code: (req: functions.https.Request, res: functions.Response<any>) => Promise<void>) =>
-  functions.https.onRequest((req, res) => {
-    if (req.method === 'OPTIONS') {
-      // Allowing CORS in complex requests like pre-flight, https://www.npmjs.com/package/cors#enabling-cors-pre-flight
-      cors({ origin: true })(req, res, () => null);
-    } else {
-      cors(corsOptionsDelegate)(req, res, async () => {
-        if (isAllowedOrigin(req)) await code(req, res);
-        else {
-          console.log('Request cancelled, origin denied');
-          res.status(404).send();
-        }
-      });
-    }
-  });
+const getAuthorizationToken = (req: Request): string => {
+  if (req.headers.authorization) return req.headers.authorization;
+  throw new Error('Unauthorized');
+};
 
-// Response helpers
-export const send = (res: functions.Response, code: string, data: string) => res.send({ code, data });
+// Checking token and if is valid, include user object in the request.
+const verifyAuthorization = async (req: Request, res: Response<any>, func: FuncSecure) => {
+  try {
+    const token = getAuthorizationToken(req);
+    const user = await admin.auth().verifyIdToken(token);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    req.user = user;
+    await func(req as RequestSecure, res);
+  } catch (e: any) {
+    res.status(401).send({ code: 'AUTHORIZATION_ERROR', data: e.message });
+  }
+};
+
+// Remove undefined props from an object
+export function removeUndefined<T extends object>(obj: T): T {
+  return pickBy(obj, negate(isUndefined)!) as T;
+}
