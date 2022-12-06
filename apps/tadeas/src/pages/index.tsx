a784764bd747166ace13476ba97b34fde37c5bac
Oliver Sosa <cundejo85@gmail.com>
2022-12-06 15:30:22 -0600

    Testing inclussion @tadeas/firestore-converters in @tadeas/webapp

--- apps/tadeas/src/pages/index.tsx
+++ apps/tadeas/src/pages/index.tsx
@@ -3,9 +3,12 @@ import { Tasks } from '@/features/task';
 import { Navbar, PageLoading } from '@/common';
 import { ListMenu, ListsDropdown, useListsLoader } from '@/features/list';
 import { styled } from '@nextui-org/react';
+import { add } from '@tadeas/firestore-converters';
 
 const IndexPage: NextPage = () => {
   const { isLoading } = useListsLoader();
+
+  console.log('add', add(5, 5));
 
   if (isLoading) return <PageLoading />;
 
