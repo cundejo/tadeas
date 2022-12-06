497cd2b21d30932da0065b9d013aa2a0ddae1e83
Oliver Sosa <cundejo85@gmail.com>
2022-12-06 17:45:31 -0600

    Added wrangler to deploy frontend to cloudinary by workflow

--- apps/tadeas/public/service-worker.js
+++ apps/tadeas/public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache_version_1670364894184';
+const CACHE_NAME = 'cache_version_1670370138192';
 const OFFLINE_URL = 'offline.html';
 
 self.addEventListener('install', function (event) {
