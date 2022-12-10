927ee2af672c9c81d02c2ab02b37d3e90dbcb1ed
Oliver Sosa <cundejo85@gmail.com>
2022-12-09 23:25:47 -0600

    Fixing bug on testing backend with empty admin initialization

--- apps/functions/src/config/index.ts
+++ apps/functions/src/config/index.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 
 dotenv.config();
 
-const SERVICE_ACCOUNT_OBJECT = JSON.parse(process.env.GCP_SA_KEY ?? '');
+const SERVICE_ACCOUNT_OBJECT = JSON.parse(process.env.GCP_SA_KEY ?? '{}');
 
 const ALLOWED_ORIGINS = [
   'https://tadeas.app',
@@ -15,9 +15,11 @@ const ALLOWED_ORIGINS = [
 
 const SENDINGBLUE_API_KEY = process.env.SENDINGBLUE_API_KEY;
 
-// The Firebase Admin SDK to access Firestore.
-admin.initializeApp({
-  credential: admin.credential.cert(SERVICE_ACCOUNT_OBJECT as any),
-});
+// The Firebase Admin SDK to access Firestore. Initialize without connection when testing.
+if (process.env.NODE_ENV === 'test') {
+  admin.initializeApp({});
+} else {
+  admin.initializeApp({ credential: admin.credential.cert(SERVICE_ACCOUNT_OBJECT as any) });
+}
 
 export { admin, ALLOWED_ORIGINS, SENDINGBLUE_API_KEY };
--- apps/tadeas/src/common/config/environment.ts
+++ apps/tadeas/src/common/config/environment.ts
@@ -1,6 +1,6 @@
 import * as versionFile from './version.json';
 
-export const NEXT_PUBLIC_FIREBASE_CONFIG = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG ?? '');
+export const NEXT_PUBLIC_FIREBASE_CONFIG = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG ?? '{}');
 export const NEXT_PUBLIC_FIREBASE_FUNCTIONS_BASE_PATH = process.env.NEXT_PUBLIC_FIREBASE_FUNCTIONS_BASE_PATH;
 
 // @ts-ignore
