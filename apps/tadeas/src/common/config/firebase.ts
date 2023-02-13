f9654827f0898cf3a6d86b762d23004d1ed71b82
Oliver Sosa <cundejo85@gmail.com>
2023-02-12 22:50:32 -0600

    Minor fix

--- apps/tadeas/src/common/config/firebase.ts
+++ apps/tadeas/src/common/config/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, Timestamp } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { DateISOString } from '@tadeas/types';
 import * as environment from './environment';
 
 const app = initializeApp(environment.NEXT_PUBLIC_FIREBASE_CONFIG);
